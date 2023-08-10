762fc6fb5a130ffc1d788ec4f4e569cbb88d0d61
Yevhenii Holodov <jekassansai720@gmail.com>
2023-08-10 16:05:48 +0200

    just desctop

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 //'use strict'
 
+const line = document.querySelectorAll('.line');
+
+line.forEach(e=>{
+  for (let i = 0; i < e.offsetWidth / 35; i++) {
+    const circle = document.createElement('div');
+    circle.classList.add('circle');
+    circle.style.left = i * 35 + 'px';
+    circle.style.backgroundColor = i % 2 === 0 ? '#CE3A42' : '#2A9F75';
+    e.appendChild(circle);
+  }
+
+})
+
+
